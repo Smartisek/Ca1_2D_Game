8778ffe987458f52df594cd113d91c3ee00a3991
Smartisek <domalipdominik@gmail.com>
2023-10-15 15:31:28 +0100

    Adding basic space and jump function

--- Assets/Scripts/PlayerScripts/playerMovement.cs
+++ Assets/Scripts/PlayerScripts/playerMovement.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
 
+// Variables for basic movement, both public so I can update inside of unity 
     public float jumpHeight = 5f;
+    public float moveSpeed = 5f;
+    private Rigidbody2D bodyRigid;
 
     // Start is called before the first frame update
     void Awake()
     {
-        
+     bodyRigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        bodyRigid.velocity = new Vector2(horizontalInput*moveSpeed, bodyRigid.velocity.y);
         
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Jump();
+        }
+
+    }
+    
+
+    void Jump(){
+        bodyRigid.velocity = new Vector2(bodyRigid.velocity.x, jumpHeight);
     }
 }
