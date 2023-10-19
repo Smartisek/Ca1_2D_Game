9fb2b42ba965df61e435b2674ae68ec1b37429e3
Smartisek <domalipdominik@gmail.com>
2023-10-19 19:37:04 +0100

    Adding a saw trap that deals damage

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -8,6 +8,7 @@ public class Health : MonoBehaviour
     [SerializeField] private float fullhealth;
     private Animator anim;
     public float currentHealth {get ; private set;}
+    private bool isDead;
 
 
     private void Awake(){
@@ -16,14 +17,18 @@ public class Health : MonoBehaviour
 
     }
 
-    private void getDamaged(float damage){
+    public void GetDamaged(float damage){
         currentHealth =  Mathf.Clamp(currentHealth - damage, 0, fullhealth);
 
         if(currentHealth > 0 ){
             // Get hurt
             anim.SetTrigger("hurt");
         } else {
+            if(!isDead){
             anim.SetTrigger("die");
+             GetComponent<PlayerMovement>().enabled = false; 
+            }
+            
         }
     } 
 
